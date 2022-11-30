0920f0baef441216a625441cef818f0fd1199094
Enem <craigcharltonlunga1000@gmail.com>
2022-11-30 15:07:53 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const x = document.getElementById("toogle__links");
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+};
